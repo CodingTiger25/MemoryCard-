c71f66defa1b7f07f40f9b7b149d855dc34fbb03
Tony <emperialace@gmail.com>
2025-10-19 21:31:43 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>     
+      <h1 className='title'>Pokemon Memory Game</h1>
+      <div className="card">
+       
+     
+      </div>
+    </>
+  )
+}
+
+export default App
