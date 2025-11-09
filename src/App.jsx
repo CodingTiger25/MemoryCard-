dfb8f66470070e286d5e0ffd7a2c11907a52b950
Tony <emperialace@gmail.com>
2025-11-09 17:29:40 -0500

    Images of pokemon shown on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,58 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
 
-  return (
-    <>     
-      <h1 className='title'>Pokemon Memory Game</h1>
-      <div className="card">
-        <p className='instructions '>Click pokemon for a point but not same in a row</p>
-       
-        <h1 className='score'>SCORE</h1>
+const [pokemon,setPokemon]=useState([]);
+const [pokeImage,setPokeImage]=useState({});
+const [points,setPoints]=useState(0);
 
-      </div>
-    </>
+
+
+useEffect(() => {
+  getPokemonData(pokemonIds);
+}, []);
+
+const pokemonIds = [104, 197, 149,395]
+
+const getPokemonData = async (pokemonIdentity) => {
+  const response = pokemonIdentity.map((id) =>  
+         fetch(`https://pokeapi.co/api/v2/pokemon/${id}`) .then((response) =>response.json()))
+  //const data = await response.json();
+  const data = await Promise.all(response);
+  console.log(data);
+  const imageUrl = data.map((pic) => pic.sprites.other['official-artwork'].front_default);
+    //data.sprites.other['official-artwork'].front_default;
+  setPokemon(imageUrl);
+}
+
+const updatePoints = () => {
+  setPoints(points + 1);
+}
+
+console.log(pokemon);
+
+  return (
+
+        <>     
+          <h1 className='title'>Pokemon Memory Game</h1>
+          <div className="card">
+            <p className='instructions '>Click pokemon for a point but not same in a row</p>
+          
+            <h1 className='score'>SCORE</h1>
+            <p>{points}</p>
+
+
+            {pokemon.map((pokemon, index) => (
+              <img key={index} src={pokemon} onClick={updatePoints} className="logo poke-logo" alt="Pokemon Logo" />
+            ))}            
+
+          </div>
+        </>
   )
 }
 
